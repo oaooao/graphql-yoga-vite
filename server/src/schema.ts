7d4c311530c12465004b6f7e525e6b425d092d93
陆伟 <oao.lu@icloud.com>
2023-02-08 16:28:39 +0800

    feat: A Simple Query

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -2,14 +2,45 @@ import { makeExecutableSchema } from '@graphql-tools/schema';
 
 const typeDefinitions = /* GraphQL */ `
   type Query {
-    hello: String!
+    info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `
 
+// 1
+type Link = {
+  id: string
+  url: string
+  description: string
+}
+ 
+// 2
+const links: Link[] = [
+  {
+    id: 'link-0',
+    url: 'https://graphql-yoga.com',
+    description: 'The easiest way of setting up a GraphQL server'
+  }
+]
+ 
 const resolvers = {
-    Query: {
-        hello: () => "Hello World"
-    }
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    // 3
+    feed: () => links
+  },
+  // 4
+  Link: {
+    id: (parent: Link) => parent.id,
+    description: (parent: Link) => parent.description,
+    url: (parent: Link) => parent.url
+  }
 }
 
 export const schema = makeExecutableSchema({
