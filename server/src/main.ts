85c4c9e00cc4f933be031d79ecc3f15ac5a5788c
陆伟 <oao.lu@icloud.com>
2023-02-08 16:15:05 +0800

    feat: GraphQL Server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,19 +1,16 @@
 import { execute, parse } from "graphql";
+import { createYoga } from "graphql-yoga";
+import { createServer } from "http";
 import { schema } from "./schema";
 
 async function main() {
-  const myQuery = parse(/* GraphQL */ `
-    query {
-      hello
-    }
-  `);
+  const yoga = createYoga({ schema });
 
-  const result = await execute({
-    schema,
-    document: myQuery,
+  const server = createServer(yoga);
+
+  server.listen(4000, () => {
+    console.log("Server is running on http://localhost:4000");
   });
-
-  console.log({ result });
 }
 
 main();
